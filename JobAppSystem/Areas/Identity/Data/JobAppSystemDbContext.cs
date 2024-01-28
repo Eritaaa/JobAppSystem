f5d98bef86f52f3dee3435fe21b25c59d949ae51
Eritaaa <ek57959@ubt-uni.net>
2024-01-28 21:47:15 +0100

    Add project files.

---
+++ JobAppSystem/Areas/Identity/Data/JobAppSystemDbContext.cs
@@ -0,0 +1,22 @@
+﻿using JobAppSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobAppSystem.Data;
+
+public class JobAppSystemDbContext : IdentityDbContext<Perdoruesi>
+{
+    public JobAppSystemDbContext(DbContextOptions<JobAppSystemDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ JobAppSystem/Areas/Identity/Data/Perdoruesi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobAppSystem.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Perdoruesi class
+public class Perdoruesi : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName ="nvarchar(100)")]
+    public string Emri { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Mbiemri { get; set; }
+}
+
