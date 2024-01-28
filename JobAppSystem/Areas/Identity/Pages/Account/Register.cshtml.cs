dbb7a9aba7be5f39d3619187feba62fd74bfa8f8
Eritaaa <ek57959@ubt-uni.net>
2024-01-29 00:45:21 +0100

    Ndryshime ne pamjen e klases Register

--- JobAppSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JobAppSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -131,6 +131,7 @@ namespace JobAppSystem.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
